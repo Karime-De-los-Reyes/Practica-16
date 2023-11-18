f6ec83e978371f8c605c1d6dbee6a7979ee16458
Dell <Dell@DELL-PC>
2023-11-18 16:49:18 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Practica_16
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("SG Alternative High-Alt", 15.7499981F, FontStyle.Italic, GraphicsUnit.Point);
+            label1.Location = new Point(110, 21);
+            label1.Name = "label1";
+            label1.Size = new Size(391, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Determina si un número es par o impar ";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("SG Alternative High-Alt", 15.7499981F, FontStyle.Italic, GraphicsUnit.Point);
+            label2.Location = new Point(73, 104);
+            label2.Name = "label2";
+            label2.Size = new Size(200, 25);
+            label2.TabIndex = 1;
+            label2.Text = "Ingresar el número ";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(305, 104);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("SG Alternative High-Alt", 15.7499981F, FontStyle.Italic, GraphicsUnit.Point);
+            button1.Location = new Point(210, 182);
+            button1.Name = "button1";
+            button1.Size = new Size(137, 32);
+            button1.TabIndex = 3;
+            button1.Text = "Determinar";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private Button button1;
+    }
+}
